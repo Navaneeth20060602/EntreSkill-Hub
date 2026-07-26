318559afb25a806d43cd18f4ad7ee3ad4cd20370
V.Navaneeth Krishna <navaneethkrishna921133@gmail.com>
2026-07-27 00:43:36 +0530

    Improve SMTP logging and trust proxy

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -1,7 +1,9 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -9,12 +11,17 @@ const transporter = nodemailer.createTransport({
 });
 
 async function sendEmail({ to, subject, body }) {
-  await transporter.sendMail({
-    from: `"EntreSkill Hub" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    text: body,
-  });
+  try {
+    await transporter.sendMail({
+      from: `"EntreSkill Hub" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      text: body,
+    });
+  } catch (err) {
+    console.error("SMTP ERROR:", err);
+    throw err;
+  }
 }
 
 module.exports = { sendEmail };
